c73188770fd30d1c362fabb16bd625ff962af804
selinagabes <gabrieleselina@gmail.com>
2016-09-22 21:37:07 -0400

    directories fixed

--- A1 - Selina/Assets/Scripts/MoveIt.cs
+++ A1 - Selina/Assets/Scripts/MoveIt.cs
@@ -5,16 +5,15 @@ using System;
 public class MoveIt : MonoBehaviour {
 
     // Use this for initialization
-    private float startTime = 0.0F;
-
+  
 	void Start () {
         //transform.position = new Vector3(transform.position.x-2, transform.position.y, transform.position.z);
 }
 	
 	// Update is called once per frame
 	void Update () {
-        
-        transform.position = new Vector3(PingPong(Time.time-startTime, 2, -2), transform.position.y, transform.position.z);
+        if(Time.time> Time.deltaTime)
+            transform.position = new Vector3(PingPong(Time.time, 2, -2), transform.position.y, transform.position.z);
 	}
 
     public float PingPong(float t,  float maxLength, float minLength)
